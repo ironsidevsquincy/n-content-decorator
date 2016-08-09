fba782792b34687a3ad3aafeabd7f3461d64fa9d
andygnewman <andrew.newman@ft.com>
2016-08-09 17:22:19 +0100

    evaluate whether premium content

--- models/basic.js
+++ models/basic.js
@@ -5,7 +5,8 @@ module.exports = content => {
 		url: content.url && content.url.replace(/https:\/\/www.ft.com/, ''),
 		title: content.title,
 		published: content.initialPublishedDate || content.published,
-		lastPublished: content.publishedDate || content.lastPublished
+		lastPublished: content.publishedDate || content.lastPublished,
+		premium: content.webUrl && /\/cms\/s\/3\//.test(content.webUrl)
 	};
 
 };
